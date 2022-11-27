46af41933e3fcfc10023cc704d8255b14c150e59
Ftc Serhiy <ftcserhiy@gmail.com>
2022-11-27 22:22:50 +0200

    add gunicorn

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM python:alpine
 COPY . app/
 WORKDIR /app
 
-RUN pip install poetry && poetry install --without dev && apk add uwsgi
+RUN pip install poetry && poetry install --without dev && apk add gunicorn
 
-CMD ["uwsgi --http 0.0.0.0:8080 --wsgi-file server/app.py --callable __hug_wsgi__"]
+# CMD ["uwsgi --http 0.0.0.0:8080 --wsgi-file server/app.py --callable __hug_wsgi__"]
---
+++ app.py
@@ -0,0 +1,23 @@
+import hug
+from hug.middleware import CORSMiddleware
+from core import download, key
+
+api = hug.API(__name__)
+api.http.add_middleware(CORSMiddleware(api))
+
+@hug.post('/')
+def index(response, body=None):
+    name = body.get('name')
+    amount = body.get('amount')
+    subject = body.get('subject')
+    klass = body.get('klass')
+
+    page = download.find(str(name), int(amount), str(subject), int(klass))
+    urls = download.get_urls(page)
+
+    return {'value': key.parse(urls[0])}
+
+@hug.get('/')
+def testsss():
+    return 'ok'
+
